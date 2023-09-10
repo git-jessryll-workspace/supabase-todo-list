10dedd1c58237b927079efc7156cbd4190f4a307
git-jessryll-workspace <62193217+git-jessryll-workspace@users.noreply.github.com>
2023-09-10 21:02:07 +0800

    add react-spinner

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 import { AuthRoute } from "./components";
 import TodoProvider from "./context/TodoProvider";
+import { ClockLoader } from "react-spinners";
 
 const AuthPage = lazy(() => import("./pages/AuthPage"));
 const TodoPage = lazy(() => import("./pages/TodoPage"));
@@ -23,7 +24,13 @@ function App() {
           element={
             <div className="mt-10">
               <main className="flex justify-center">
-                <Suspense fallback={<div>loading...</div>}>
+                <Suspense
+                  fallback={
+                    <div className="flex justify-center h-[300px] items-center">
+                      <ClockLoader color="#36d7b7"/>
+                    </div>
+                  }
+                >
                   <TodoProvider>
                     <TodoPage />
                   </TodoProvider>
